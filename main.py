b3a6d7c4019d7a74f211a30ca3ba8d65c284453c
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-11-21 11:35:12 +0000

    v0.2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from functions import *
 
 # Nom de fichier à charger
-fileName = "";
+fileName = "FIFA_World_Cup/FIFA-2022.txt";
 
 # Charge un fichier en lecture
 file = open(fileName, "r")
 
 # Charge les données à partir d'un fichier
 data = load_data(fileName);
+
 
