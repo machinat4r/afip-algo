8960997c415e6e1400078b28228de88a3fbca8bd
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-12-08 18:33:46 +0000

    4.5

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
 
 import os
+#charge les donné a partir d'un fichier
+def load_data(fileName):
+    f = open(fileName,"r")
+    #print(f.read())
+    data = [];
+    for x in f:
+        data += [x];
+    return data;
+
--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from functions import *
 # Nom de fichier à charger
 fileName = "FIFA_World_Cup/FIFA-2022.txt";
 
-# Charge un fichier en lecture
+
+manger = "FIFA_World_Cup/manger.txt"
+
+#Charge un fichier en lecture
 file = open(fileName, "r")
 
 # Charge les données à partir d'un fichier
-data = load_data(fileName);
+#data = load_data(fileName);
 
+
+liste_raw = lire_fichier(fileName)
+
+
+#liste_pur = supprimer_ligne(liste_raw)
+
+print(liste_raw)
 
