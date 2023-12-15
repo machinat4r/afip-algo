247f0f29841d8e2aaec17b2cf77393ef7d14cc18
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-12-15 21:37:49 +0000

    2.2

--- 2.2.py
+++ 2.2.py
@@ -1,21 +1,66 @@
+import pandas as pd
+
 fichier = open("FIFA_World_Cup/FIFA-2022.txt", "r")
 
-
-liste_vide = []
-
+with open('FIFA_World_Cup/FIFA-2022.txt', 'r') as f:
+    lines = f.readlines()
 
 f = fichier.readlines()
 
+liste = []
+
+
+for line in lines:
+    columns = line.strip().split(',')
+    liste.append(columns)
+
+
+mactrice = pd.DataFrame(liste)
+
+
+del liste[0]
+
+
+mactrice = pd.DataFrame(liste, columns=['Position', 'Team', 'Games Played', 'Win', 'Draw', 'Loss', 'Goals For', 'Goals Against', 'Goal Difference', 'Points'])
+
+
+mactrice['Win'] = mactrice['Win'].astype('int')
+mactrice['Draw'] = mactrice['Draw'].astype('int')
+
+
+mactrice['Calcul'] = 3 * mactrice['Win'] + mactrice['Draw']
+
+pts = mactrice['Calcul']
+
+pts_liste = pts.to_list()
+
+pts_liste = [str(element)for element in pts_liste]
+
+
+def recup_ekip(ligne):
+
+
+    sepa = ","
+    acc = 0
+    sequence = []
+
+
+    for pos_actu in ligne:
+        if pos_actu == sepa:
+            acc = acc + 1
+        if acc > 0 and acc <= 1 and pos_actu != sepa:
+            sequence += pos_actu
+    return sequence[0] + sequence[1] + sequence[2]
+    
+
+liste_ekip = []
+
 
 for ligne in f:
-    if(ligne[-1] == "\n"):
-        liste_vide.append(ligne[:-1])
-    else:
-        liste_vide.append(ligne)
+    liste_ekip.append(recup_ekip(ligne))
+
+del liste_ekip[0]
 
 
-del liste_vide[0]
-
-
-tpl = [pays[2:5] for pays in liste_vide]
-print(tpl)
+for equipe in liste_ekip:
+    print(equipe + ":" + " " + pts_liste[liste_ekip.index(equipe)] + " " + "pts")
