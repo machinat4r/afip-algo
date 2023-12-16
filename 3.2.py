1eb7e79e5b5b37e8c2664834954393b8baca4ea5
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-12-16 14:07:28 +0000

    3.2

---
+++ 3.2.py
@@ -0,0 +1,34 @@
+fichier = open("FIFA_World_Cup/FIFA-2022.txt", "r")
+
+f = fichier.readlines()
+
+
+def recup_ekip(ligne):
+
+
+    sepa = ","
+    acc = 0
+    sequence = []
+
+
+    for pos_actu in ligne:
+        if pos_actu == sepa:
+            acc = acc + 1
+        if acc > 0 and acc <= 1 and pos_actu != sepa:
+            sequence += pos_actu
+    return sequence[0] + sequence[1] + sequence[2]
+    
+
+liste_ekip = []
+
+
+for ligne in f:
+    liste_ekip.append(recup_ekip(ligne))
+
+del liste_ekip[0]
+
+liste_tri = sorted(liste_ekip)
+
+
+for equipe in liste_tri:
+    print(equipe)
